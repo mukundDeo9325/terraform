887b98bce49288f21e56a6724a29f61c68cd6261
Mukund Deotale <mukundeotale9860@gmail.com>
2026-04-23 18:14:20 +0530

    Update backend.tf

--- terraform-project/backend.tf
+++ terraform-project/backend.tf
@@ -1,7 +1,10 @@
- bucket = "s3terraformbalt89i-${terraform.workspace}"
-  acl    = "private"
 
-  tags = {
-    Name = "workspace-demo"
+terraform {
+  backend "s3" {
+    bucket         = "s3terraformbalt89i-${terraform.workspace}" # Replace with your bucket name
+    key            = "environment-name/terraform.tfstate"  # Path within the bucket
+    region         = "us-east-1"  # Replace with your AWS region
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"  # Optional for state locking
   }
 }
