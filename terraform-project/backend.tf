cd0f709816e6b6f0e0d3e91be4ca75e3c18a2af5
Mukund Deotale <mukundeotale9860@gmail.com>
2026-04-23 18:15:01 +0530

    Update backend.tf

--- terraform-project/backend.tf
+++ terraform-project/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "s3terraformbalt89i-${terraform.workspace}" # Replace with your bucket name
-    key            = "environment-name/terraform.tfstate"  # Path within the bucket
+    key            = "${terraform.workspace}-terraform.tfstate"  # Path within the bucket
     region         = "us-east-1"  # Replace with your AWS region
     encrypt        = true
     dynamodb_table = "terraform-lock-table"  # Optional for state locking
